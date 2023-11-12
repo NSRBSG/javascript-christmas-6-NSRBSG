6b2d214f21709e68a092f5a5f3424be523069ea6
NSRBSG <nsrbsg@gmail.com>
2023-11-12 19:53:07 +0900

    프로그램 시작점 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import EventPlanner from './EventPlanner.js';
+
 class App {
-  async run() {}
+  async run() {
+    const eventPlanner = new EventPlanner();
+    await eventPlanner.getDateToVisit();
+    await eventPlanner.getMenu();
+    eventPlanner.previewBenefit();
+  }
 }
 
 export default App;
